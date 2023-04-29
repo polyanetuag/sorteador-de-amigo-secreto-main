59933c466a646c531fb8cceff7a7ee824db4465b
Polyane Tuag <pollyportugues@hotmail.com>
2023-04-29 10:22:18 -0300

    feat: added Cabecalho component

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import Cabecalho from './components/Cabecalho';
+// 
 ReactDOM.render(
   <React.StrictMode>
+    <Cabecalho />        
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-reportWebVitals();
