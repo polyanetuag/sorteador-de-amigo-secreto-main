a78e6878522cd73e65ee7a4ecc7634e25f1b11b3
Polyane Tuag <pollyportugues@hotmail.com>
2023-04-29 10:20:35 -0300

    feat: Formulario component

---
+++ src/components/Formulario.tsx
@@ -0,0 +1,10 @@
+const Formulario = () => {
+    return (
+        <form>
+            <input type="text" placeholder="Insira os nomes dos participantes" />
+            <button type="submit" disabled>Adicionar</button>
+        </form>
+    )
+}
+
+export default Formulario
