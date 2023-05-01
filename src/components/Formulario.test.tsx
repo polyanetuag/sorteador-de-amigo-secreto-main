7e7988b9150759b7800d1b7404190ee3ea32393e
Polyane Tuag <pollyportugues@hotmail.com>
2023-05-01 10:09:02 -0300

    feat: test when submitting a new participant

--- src/components/Formulario.test.tsx
+++ src/components/Formulario.test.tsx
@@ -1,19 +1,49 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import Formulario from "./Formulario";
+import { RecoilRoot } from "recoil";
 
-test('quando o input está vazio, novos participantes não podem ser adicionados', () => {
-    render(<Formulario />)
+test("quando o input está vazio, novos participantes não podem ser adicionados", () => {
+  render(<Formulario />);
 
-    //encontrar no DOM o input
-    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+  //encontrar no DOM o input
+  const input = screen.getByPlaceholderText(
+    "Insira os nomes dos participantes"
+  );
 
-    //encontrar o botão
-    const botao = screen.getByRole('button')
+  //encontrar o botão
+  const botao = screen.getByRole("button");
 
-    //garantir que o input esteja no documento
-    expect(input).toBeInTheDocument()
+  //garantir que o input esteja no documento
+  expect(input).toBeInTheDocument();
 
-    //garantir que o botão esteja desabilitado
-    expect(botao).toBeDisabled()
-})
+  //garantir que o botão esteja desabilitado
+  expect(botao).toBeDisabled();
+});
+
+test("adicionar um participante caso exista um nome preenchido", () => {
+  render(
+    <RecoilRoot>
+      <Formulario />
+    </RecoilRoot>
+  );
+  const input = screen.getByPlaceholderText(
+    "Insira os nomes dos participantes"
+  );
+  const botao = screen.getByRole("button");
+
+  // inserir um valor no input
+  fireEvent.change(input, {
+    target: {
+      value: "Maria",
+    },
+  });
+  // clicar no botão de submeter
+  fireEvent.click(botao);
+
+  // garantir que o input esteja com o foco ativo
+  expect(input).toHaveFocus();
+
+  // garantir que o input não tenha um valor
+  expect(input).toHaveValue("");
+});
