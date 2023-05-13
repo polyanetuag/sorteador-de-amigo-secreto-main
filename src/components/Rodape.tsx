c1ac63463c605eda29f13133d2535955ab62e2b8
Polyane Tuag <pollyportugues@hotmail.com>
2023-05-13 10:35:36 -0300

    fix: structure Rodape

--- src/components/Rodape.tsx
+++ src/components/Rodape.tsx
@@ -1,15 +1,22 @@
-// import { useNavigate } from "react-router-dom";
-import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes";
+import { useNavigate } from "react-router-dom"
+import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes"
+
+import './Rodape.css'
 
 const Rodape = () => {
-const participantes = useListaDeParticipantes
 
-// const navegarPara = useNavigate
+    const participantes = useListaDeParticipantes()
 
-    return (
-        <footer>
-        <button disabled={participantes.length < 3}>Iniciar brincadeira</button>
-        </footer>
-    );
-    };
-export default Rodape;
+    const navegarPara = useNavigate()
+    
+    const iniciar = () => {
+        navegarPara('/sorteio')
+    }
+
+    return (<footer className="rodape-configuracoes">
+        <button className="botao" disabled={participantes.length < 3} onClick={iniciar}>Iniciar brincadeira</button>
+        <img src="/imagens/sacolas.png" alt="Sacolas de compras" />
+    </footer>)
+}
+
+export default Rodape
