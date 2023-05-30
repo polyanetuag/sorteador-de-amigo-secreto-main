9bf881ac712987c046f2ad4e4472c39418be5c94
Polyane Tuag <pollyportugues@hotmail.com>
2023-05-30 10:22:02 -0300

    feat: test for prize draw

--- src/paginas/Sorteio.test.tsx
+++ src/paginas/Sorteio.test.tsx
@@ -1,20 +1,33 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { RecoilRoot } from "recoil";
 import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes";
 import Sorteio from "./Sorteio";
+import { useResultadoSorteio } from "../state/hooks/useResultadoSorteio";
 
 jest.mock("../state/hooks/useListaDeParticipantes", () => {
   return {
     useListaDeParticipantes: jest.fn(),
   };
 });
+jest.mock("../state/hooks/useResultadoSorteio", () => {
+  return {
+    useResultadoSorteio: jest.fn(),
+  };
+});
 
 describe("na pagina de sorteio", () => {
   const participantes = ["Ana", "Maria", "Joana"];
 
+  const resultado = new Map([
+    ['Ana', 'Joana'],
+    ['Joana', 'Maria'],
+    ['Maria', 'Ana'],
+  ])
+
   beforeEach(() => {
     (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes);
+    (useResultadoSorteio as jest.Mock).mockReturnValue(resultado);
   });
 
   test("todos os participantes podem exibir o seu amigo secreto", () => {
@@ -27,4 +40,25 @@ describe("na pagina de sorteio", () => {
     const opcoes = screen.queryAllByRole("option");
     expect(opcoes).toHaveLength(participantes.length);
   });
+
+  test('o amigo secreto é exibido quando solicitado', () => {
+    render(
+      <RecoilRoot>
+        <Sorteio />{" "}
+      </RecoilRoot>
+    );
+
+    const select = screen.getByPlaceholderText('Selecione o seu nome')
+    fireEvent.change(select, {
+      target: {
+        value: participantes[0]
+      }
+    })
+
+    const botao = screen.getByRole('button')
+    fireEvent.click(botao)
+
+    const amigoSecreto = screen.getByRole('alert')
+    expect(amigoSecreto).toBeInTheDocument()
+  })
 });
