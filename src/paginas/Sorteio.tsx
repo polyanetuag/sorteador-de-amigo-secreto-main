7a71fa24ffe34950b5f483152c44c67b4ba67715
Polyane Tuag <pollyportugues@hotmail.com>
2023-05-30 10:42:26 -0300

    feat: added styles

--- src/paginas/Sorteio.tsx
+++ src/paginas/Sorteio.tsx
@@ -1,39 +1,54 @@
-import { useState } from "react"
-import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes"
-import { useResultadoSorteio } from "../state/hooks/useResultadoSorteio"
+import { useState } from "react";
+import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes";
+import { useResultadoSorteio } from "../state/hooks/useResultadoSorteio";
+import Card from "../components/Card";
+
+import './Sorteio.css'
 
 const Sorteio = () => {
-    const participante = useListaDeParticipantes()
-    const [participanteDaVez, setParticipanteDaVez] = useState('')
-    const [amigoSecreto, setAmigoSecreto] = useState('')
+  const participante = useListaDeParticipantes();
+  const [participanteDaVez, setParticipanteDaVez] = useState("");
+  const [amigoSecreto, setAmigoSecreto] = useState("");
 
-    const resultado = useResultadoSorteio()
+  const resultado = useResultadoSorteio();
 
-    const sortear = (evento:React.FormEvent<HTMLFormElement>) => {
-        evento.preventDefault()
-        if(resultado.has(participanteDaVez)) {
-            setAmigoSecreto(resultado.get(participanteDaVez)!)
-
-        }
+  const sortear = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+    if (resultado.has(participanteDaVez)) {
+      setAmigoSecreto(resultado.get(participanteDaVez)!);
     }
+  };
 
-    return (
-        <section>
-            <form onSubmit={sortear}>
-                <select 
-                required 
-                name="participanteDaVez" 
-                id="participanteDaVez" 
-                placeholder="Selecione o seu nome" 
-                value={participanteDaVez} 
-                onChange={evento => setParticipanteDaVez(evento.target.value)}>
-                    {participante.map(participante => <option key={participante}>{participante}</option>)}
-                </select>
-                <button>Sortear</button>
-            </form>
-            {amigoSecreto && <p role="alert">{amigoSecreto}</p>}
-        </section>
-    )
-}
+  return (
+    <Card>
+      <section className="sorteio">
+        <h2 className="sorteio-title">Quem vai tirar o papelzinho?</h2>
+        <form onSubmit={sortear}>
+          <select
+            required
+            name="participanteDaVez"
+            id="participanteDaVez"
+            placeholder="Selecione o seu nome"
+            value={participanteDaVez}
+            onChange={(evento) => setParticipanteDaVez(evento.target.value)}
+          >
+            {participante.map((participante) => (
+              <option key={participante}>{participante}</option>
+            ))}
+          </select>
+          <button className="botao-sortear">Sortear</button>
+        </form>
+        {amigoSecreto && <p className="resultado" role="alert">{amigoSecreto}</p>}
+        <footer className="sorteio">
+          <img
+            src="/images/aviao.png"
+            className="aviao"
+            alt="Um desenho de um avião de papel"
+          />
+        </footer>
+      </section>
+    </Card>
+  );
+};
 
-export default Sorteio
+export default Sorteio;
